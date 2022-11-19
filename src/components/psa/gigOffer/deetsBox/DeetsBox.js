c14b80a5cd5efeab089869fd0d5901f355cd3d28
kfiddle <kenjfiddle@gmail.com>
2022-11-19 15:57:36 -0500

    small change in deetsBox

--- src/components/psa/gigOffer/deetsBox/DeetsBox.js
+++ src/components/psa/gigOffer/deetsBox/DeetsBox.js
@@ -3,8 +3,7 @@ import Service from "./service/Service";
 
 import styles from "./DeetsBox.module.css";
 
-const DeetsBox = (props) => {
-  const services = props.services;
+const DeetsBox = ({ services }) => {
 
   const displayableServices = services.map((service) => (
     <Service key={services.indexOf(service)} service={service}/>
